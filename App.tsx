770884337023ca1d2c90849ecc7e0def6f1c92db
Rafael Nicdao <nicdaoraf@gmail.com>
2024-05-03 21:00:21 +1000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export default () => (
+  <>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </>
+);
